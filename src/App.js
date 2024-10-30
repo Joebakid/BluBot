d2accde2c08bd9e5ba1fafe9d2dd9792d023374d
joebakid <josephbawo@gmail.com>
2024-10-30 23:13:54 +0100

    sap opacity

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import About from "./components/About";
@@ -8,8 +8,22 @@ import Tokenomics from "./components/Tokenomics";
 import HowToBuy from "./components/HowToBuy";
 import FAQs from "./components/FAQs";
 import Roadmap from "./components/Roadmap";
+import Preloader from "./components/Preloader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading delay (e.g., 2 seconds)
+    const timer = setTimeout(() => setLoading(false), 5000);
+
+    return () => clearTimeout(timer); // Cleanup the timer
+  }, []);
+
+  if (loading) {
+    return <Preloader />;
+  }
+
   return (
     <div className="App">
       <div className="bg-img">
