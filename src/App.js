becd9ad3c057b01b3e6654fe15354ae9d3f2ea32
joebakid <josephbawo@gmail.com>
2024-10-30 23:39:27 +0100

    gsap bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { Power3 } from "gsap";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import About from "./components/About";
@@ -12,13 +14,25 @@ import Preloader from "./components/Preloader";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const navRef = useRef(null); // Reference for NavBar animation
 
   useEffect(() => {
-    // Simulate a loading delay (e.g., 2 seconds)
+    // Simulate a loading delay (e.g., 5 seconds)
     const timer = setTimeout(() => setLoading(false), 5000);
 
     return () => clearTimeout(timer); // Cleanup the timer
   }, []);
+
+  useEffect(() => {
+    if (!loading) {
+      const tl = gsap.timeline();
+      tl.fromTo(
+        navRef.current,
+        { opacity: 0, y: "-10%" },
+        { opacity: 1, y: "0%", duration: 3, delay: 0.2, ease: Power3.easeOut }
+      );
+    }
+  }, [loading]);
 
   if (loading) {
     return <Preloader />;
@@ -27,7 +41,7 @@ function App() {
   return (
     <div className="App">
       <div className="bg-img">
-        <NavBar />
+        <NavBar ref={navRef} /> {/* Pass ref to NavBar */}
         <Header />
         <About />
       </div>
